70ed0fc5e764cdd2288297fb8d7fe2ae6c510dc2
Sowamy <adlefevers2@yahoo.com>
2017-09-06 09:50:03 -0500

    Update Lab1Problem1.c

--- Lab1Problem1.c
+++ Lab1Problem1.c
@@ -8,15 +8,27 @@ float getCircleArea( float radius );
 int main( void )
 {
 	float radius = 0;
+	short positiveTest = 0;	// Flag: 1 if the radius is greater than or equal to 0: 0 otherwise.
 
-	// Prompt to user to enter radius, and assigns valued entered into float radius
-	printf( "Enter radius of circle: \n--> " );
-	scanf( "%f", &radius );
+	// Will continue to prompt user if the radius entered is not a valid value
+	while( positiveTest != 1 ) {
 
-	// TODO radius check - negative
+		positiveTest = 1;
 
-	// Displays the area of the circle to the user
-	printf( "\n\nThe area of the circle is: %.4f", getCircleArea(radius) );
+		// Prompt to user to enter radius, and assigns valued entered into float radius
+		printf( "\nEnter radius of circle: \n--> " );
+		scanf( "%f", &radius );
+
+		// TODO radius check - negative
+		if( radius < 0 ) {
+			printf( "\nERROR: Radius must be a number greater than or equal to zero\n" );
+			positiveTest = 0;
+		} // END if : Negative Test
+
+	} // END while : Positivity Test
+
+	// Displays the area of the circle to the user, accurate to 4 decimal places
+	printf( "\nThe area of the circle is: %.4f\n\n", getCircleArea(radius) );
 
 	return 0;
 } // END FUNCTION main
