83c245e3a8a86fde5cd4d13503aaaa78f4c84fde
Sowamy <adlefevers2@yahoo.com>
2017-09-13 11:17:42 -0500

    Update L6_Ex1_allocation.c

--- Examples/L6_Ex1_allocation.c
+++ Examples/L6_Ex1_allocation.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
-int x = 0;
+
+int x = 0;			// Global Variable
 
 int f(void){
-	int x = 10;
+	int x = 10;		// Local variable x
 	x = x + 10;
 	return x;
 }
 
 int g(void){
-	static int x = 10;
-	x = x + 10;
+	static int x = 10;	// Binds x to global variable x
+	x = x + 10;		// Adds 10 to global variable x
 	return x;
 }
 
 double h(double y){
-	y += (double)(++x);
+	y += (double)(++x);	// Adds input y to global variable x (preincremented by 1)
 	return y;
 }
 
 int main(void){
-	printf("%04d \n", f());
-    printf("%04d \n", g());
-	printf("%.2f \n", h(2.5));
-	printf("%04d \n", f());
-	printf("%04d \n", g());
-	printf("%.2f \n", h(2.5));
+	printf("%04d \n", f());		// Output: 0020
+   	printf("%04d \n", g());		// Output: 0020
+	printf("%.2f \n", h(2.5));	// Output: 3.50
+	printf("%04d \n", f());		// Output: 0020
+	printf("%04d \n", g());		// Output: 0030
+	printf("%.2f \n", h(2.5));	// Output: 4.50
 	return 0;
 }
